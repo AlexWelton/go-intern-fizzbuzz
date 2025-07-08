c49900458221309592207538136ba3fcc2eaa12d
Alex Welton <Alex.Welton@softwire.com>
2025-07-08 13:45:21 +0100

    Bang Implemented

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func fizzBuzz(number int) string {
+	var messageBuilder strings.Builder
+	if number%3 == 0 {
+		messageBuilder.WriteString("Fizz")
+	}
+
+	if number%5 == 0 {
+		messageBuilder.WriteString("Buzz")
+	}
+
+	if number%7 == 0 {
+		messageBuilder.WriteString("Bang")
+	}
+
+	var result string = messageBuilder.String()
+
+	if result == "" {
+		return strconv.Itoa(number)
+	} else {
+		return result
+	}
+}
 
 // This is our main function, this executes by default when we run the main package.
 func main() {
-	fmt.Println("Hello, World!")
 
-	// Put your code here...
+	for i := 1; i <= 100; i++ {
+		fmt.Println(fizzBuzz(i))
+	}
 }
