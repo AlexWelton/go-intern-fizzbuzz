48e8dfd8c92ac60ae6d9c5c52ee6b8f73da37abf
Alex Welton <Alex.Welton@softwire.com>
2025-07-08 13:45:31 +0100

    Testing

---
+++ main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+
+var fizzBuzzTests = []struct {
+	input    int
+	expected string
+}{
+	{1, "1"},
+	{2, "2"},
+	{3, "Fizz"},
+	{5, "Buzz"},
+	{15, "FizzBuzz"},
+	{7, "Bang"},
+	{21, "FizzBang"},
+	{35, "BuzzBang"},
+	{105, "FizzBuzzBang"},
+}
+
+func TestFizzBuzz(t *testing.T) {
+	for _, test := range fizzBuzzTests {
+		result := fizzBuzz(test.input)
+		if result != test.expected {
+			t.Errorf("incorrect greeting, for name %s: got %s, expected %s", strconv.Itoa(test.input), result, test.expected)
+		}
+	}
+}
